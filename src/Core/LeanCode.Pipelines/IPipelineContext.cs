18ade591639506142d9c3a72ce608373bbc8ebdb
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2019-11-08 16:45:20 +0100

    Make IPipelineScope.Scope nullable

--- src/Core/LeanCode.Pipelines/IPipelineContext.cs
+++ src/Core/LeanCode.Pipelines/IPipelineContext.cs
@@ -7,17 +7,6 @@ namespace LeanCode.Pipelines
         /// <summary>
         /// Managed by <see cref="PipelineExecutor{TContext, TInput, TOutput}" />.
         /// </summary>
-        IPipelineScope Scope { get; set; }
-    }
-
-    public class PipelineContext : IPipelineContext
-    {
-        private IPipelineScope? scope;
-
-        public IPipelineScope Scope
-        {
-            get => scope ?? throw new NullReferenceException();
-            set => scope = value;
-        }
+        IPipelineScope? Scope { get; set; }
     }
 }
